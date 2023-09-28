55bbc95a61bb182ab8cee1cd640cd633f3929210
Elad Ben-David <eladbe@checkpoint.com>
2023-09-28 10:18:32 +0300

    few notes

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import logging
 #This block of code handling the JSON Multi-line logs for CloudWatch.
 #https://stackoverflow.com/questions/50233013/aws-lambda-logs-to-one-json-line
 class FormatterJSON(logging.Formatter):
+# This code formats the log record as a JSON object with the specific fields, 
+# and returns the final JSON object as a result:
     def format(self, record):
         record.message = record.getMessage()
         if self.usesTime():
@@ -18,7 +20,8 @@ class FormatterJSON(logging.Formatter):
         }
         return json.dumps(j)
 
-
+# Setting the below will make a log record with the level of INFO and higher, and will be formated 
+# as a JSON object based on the above.
 logger = logging.getLogger()
 logger.setLevel('INFO')
 
@@ -32,19 +35,20 @@ logger.handlers[0].setFormatter(formatter)
 #This block of code is running by the Lambda function, when there is a trigger.
 def lambda_handler(event, context):
 
-#Parsing the event data to JSON format:
+#Parsing the event data to JSON format(string -->JSON):
     body_data = json.loads(event['body'])
 
 #Checking for the state of the PR if it was merged.
 #If it does, we are starting to get the information we need using GIT_API.
     if 'action' in body_data:
         if body_data['action'] == 'closed' and body_data['pull_request']['merged']:
-            
+            # setting the required params for accssing the PR event:
             repo_name = os.environ.get("REPO_NAME")
             git_token = os.environ.get("GIT_PAT")
             pr_number = body_data['number']
             repo_full_name = body_data["pull_request"]["head"]["repo"]["full_name"]
             print(f'The pull request #{pr_number} to {repo_name} was merged!')
+            # Using GIT API to get the PR event info:
             url = f'https://api.github.com/repos/{repo_full_name}/pulls/{pr_number}/files'
 
             headers = {
@@ -53,6 +57,9 @@ def lambda_handler(event, context):
                 'X-GitHub-Api-Version': '2022-11-28'
             }
             response = requests.get(url, headers=headers)
+            # These lines setting the response as JSON format, formating the GIT API response to JSON object
+            # while setting an indentation of 4 spaces and supports non-ASCII charts.
+            # Then, we are formatting the output back to String Python object to itterate it using "for" loop.
             response_json = response.json()
             changed_files = json.dumps(response_json, indent=4, ensure_ascii=False)
             changed_files = json.loads(changed_files)
